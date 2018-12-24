0e90ce598598d0db60a247972cd0c1c9899c2683
tylerph <=>
2018-12-24 11:22:26 -0500

    keras constructor updated

---
+++ construct/precache-manifest.11b9d0460c249327b603353f2e0c7983.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c6febb6c2f2f9a6bace",
+    "url": "/construct/static/css/main.fd6782ef.chunk.css"
+  },
+  {
+    "revision": "9c6febb6c2f2f9a6bace",
+    "url": "/construct/static/js/main.9c6febb6.chunk.js"
+  },
+  {
+    "revision": "26c9ce4a90ca48100638",
+    "url": "/construct/static/js/1.26c9ce4a.chunk.js"
+  },
+  {
+    "revision": "29056d05b48a59decdcf",
+    "url": "/construct/static/js/runtime~main.29056d05.js"
+  },
+  {
+    "revision": "1d0be7a67506ccc0a648ff728cf12515",
+    "url": "/construct/static/media/add-icon.1d0be7a6.svg"
+  },
+  {
+    "revision": "65cb6e2dfcfe54969267de4ebfb2f2a1",
+    "url": "/construct/static/media/subitem-icon.65cb6e2d.svg"
+  },
+  {
+    "revision": "83293fb7928d92205c4b9399f204665f",
+    "url": "/construct/static/media/arrow-up.83293fb7.svg"
+  },
+  {
+    "revision": "0cf9f8b36a74d7be2634a294eee04f12",
+    "url": "/construct/static/media/arrow-down.0cf9f8b3.svg"
+  },
+  {
+    "revision": "01f80f3e65872c84c582609df7adb35a",
+    "url": "/construct/static/media/activation-icon.01f80f3e.svg"
+  },
+  {
+    "revision": "65eaab1877bd0a684b56e5e973d8f843",
+    "url": "/construct/static/media/conv-icon.65eaab18.svg"
+  },
+  {
+    "revision": "35d4f30a2188322794678d2849e2f4a3",
+    "url": "/construct/static/media/dense-icon.35d4f30a.svg"
+  },
+  {
+    "revision": "d0e59bea6eb7d0037997a30f9584d6bc",
+    "url": "/construct/static/media/flatten-icon.d0e59bea.svg"
+  },
+  {
+    "revision": "b7b9b2d89fe13bcc213692eec21647f5",
+    "url": "/construct/static/media/noise-icon.b7b9b2d8.svg"
+  },
+  {
+    "revision": "58fea31ab70a3059f2c34f1320c0aab9",
+    "url": "/construct/static/media/pool-icon.58fea31a.svg"
+  },
+  {
+    "revision": "dfcb75135b227ae9385e074fb1e1c0d3",
+    "url": "/construct/static/media/recurrent-icon.dfcb7513.svg"
+  },
+  {
+    "revision": "92fed1fb006ca296d69f6153bf2a424b",
+    "url": "/construct/static/media/start-icon.92fed1fb.svg"
+  },
+  {
+    "revision": "b8c4771cb84605f61eb721307453886f",
+    "url": "/construct/static/media/trash-icon.b8c4771c.svg"
+  },
+  {
+    "revision": "6d3115cae79ac576387f9f9a7cda8be5",
+    "url": "/construct/index.html"
+  }
+];
